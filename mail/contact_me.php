db41db783bbcaa040f488a499e815d968d3f48e8
VJ <vjagadeesan@esri.com>
2017-03-08 18:25:59 -0800

    contact form

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -11,6 +11,7 @@ if(empty($_POST['name'])  		||
    }
 	
 $name = strip_tags(htmlspecialchars($_POST['name']));
+$company = strip_tags(htmlspecialchars($_POST['company']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
@@ -18,7 +19,7 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 // Create the email and send the message
 $to = 'info@chargebackpatrols.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $company\n\nCompany: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: noreply@ychargebackpatrols.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
